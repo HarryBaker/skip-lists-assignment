d6d054021e77439ae10ab766b07f80ee521eb0bc
boatenga17 <boatenga17@grinnell.edu>
2014-11-11 23:02:00 -0600

    Add some implementation to the SkipList. Check add(T val) and make the necessary corrections

--- src/taojava/util/SkipList.java
+++ src/taojava/util/SkipList.java
@@ -1,9 +1,13 @@
 package taojava.util;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Random;
 
 /**
  * A randomized implementation of sorted lists.  
+ * 
+ * Consulted code from Patrick Slough's repository
  * 
  * @author Samuel A. Rebelsky
  * @author Your Name Here
@@ -14,6 +18,12 @@ public class SkipList<T extends Comparable<T>>
   // +--------+----------------------------------------------------------
   // | Fields |
   // +--------+
+  int maxLevel = 20;
+  int size;
+  Random rand;
+  Node header;
+  Node dummy;
+  double p = 0.5;
 
   // +------------------+------------------------------------------------
   // | Internal Classes |
@@ -32,15 +42,50 @@ public class SkipList<T extends Comparable<T>>
      * The value stored in the node.
      */
     T val;
+    int level;
+    Node[] nextArray; // storing all pointers to the different levels 
+
+    @SuppressWarnings("unchecked")
+    public Node(T val, int level)
+    {
+      this.val = val;
+      this.level = level;
+      this.nextArray = new SkipList.Node[level];
+    }// Node(T, int)
   } // class Node
 
   // +--------------+----------------------------------------------------
   // | Constructors |
   // +--------------+
 
+  public SkipList()
+  {
+    this.header = new Node(null, maxLevel);
+    this.dummy = new Node(null, maxLevel);
+    this.size = 0;
+
+    for (int i = 0; i <= this.maxLevel; i++)
+      {
+        this.header.nextArray[i] = this.dummy;
+        this.dummy.nextArray[i] = null;
+      }// for
+
+  }// SkipList()
+
   // +-------------------------+-----------------------------------------
   // | Internal Helper Methods |
   // +-------------------------+
+
+  public int randomHeight(double p)
+  {
+    this.rand = new Random();
+    int level = 0;
+    while (this.rand.nextDouble() < p)
+      {
+        level++;
+      }
+    return Math.min(level, this.maxLevel);
+  }
 
   // +-----------------------+-------------------------------------------
   // | Methods from Iterable |
@@ -53,8 +98,33 @@ public class SkipList<T extends Comparable<T>>
    */
   public Iterator<T> iterator()
   {
-    // STUB
-    return null;
+    return new Iterator<T>()
+      {
+        Node cursor = SkipList.this.header;
+
+        @Override
+        public boolean hasNext()
+        {
+          return cursor.nextArray[0] != null;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public T next()
+        {
+          if (!hasNext())
+            {
+              throw new NoSuchElementException(
+                                               "There is no element after this node");
+            }
+          else
+            {
+              cursor = cursor.nextArray[0];
+            }
+          return cursor.val;
+        }
+      };
+
   } // iterator()
 
   // +------------------------+------------------------------------------
@@ -70,7 +140,44 @@ public class SkipList<T extends Comparable<T>>
    */
   public void add(T val)
   {
-    // STUB
+    int counter = this.maxLevel;
+    Node temp = this.header;
+    int height = randomHeight(this.p);
+    Node insert = new Node(val, height);
+    Node[] tracker = new SkipList.Node[this.maxLevel];
+    
+   /* while (this.header.nextArray[counter].val.compareTo(val) >= 0
+        || this.header.nextArray[counter].val == null){
+     counter--;
+     if (this.header.nextArray[counter].val != null)
+       {
+         
+       }
+    }
+    */
+    while (temp.nextArray[counter].val.compareTo(val) >= 0
+        || temp.nextArray[counter] == null)
+      {
+       tracker[counter] = temp.nextArray[counter];
+       
+        if (temp.nextArray[counter] != null)
+          {
+            temp = temp.nextArray[counter];
+          }
+        counter--;
+      }
+    
+    int countToZero = counter;
+    for (int i = countToZero; i >= 0; i--)
+      {
+        tracker[i] = temp.nextArray[i];
+      }
+    
+    for (int i = 0; i < height; i ++)
+      {
+        temp.nextArray[i] = insert;
+      }
+    
   } // add(T val)
 
   /**
@@ -118,6 +225,5 @@ public class SkipList<T extends Comparable<T>>
     // STUB
     return 0;
   } // length()
-
 
 } // class SkipList<T>
